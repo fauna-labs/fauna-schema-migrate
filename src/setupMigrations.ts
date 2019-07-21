5bcb1a931fc4cfcf9bc0e6b0e9e239fcd17c7e24
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-07-21 12:32:11 -0300

    Check if setup was done

--- src/setupMigrations.ts
+++ src/setupMigrations.ts
@@ -4,6 +4,13 @@ import chalk from "chalk";
 const setupMigrations = async (client: Client) => {
   try {
     console.log("Setup migrations...");
+
+    const hasSetup = await client.query(q.Exists(q.Collection("Migration")));
+    if (hasSetup) {
+      console.log("Setup is already done.");
+      return;
+    }
+
     await client.query(q.CreateClass({ name: "Migration" }));
     await client.query(
       q.CreateIndex({ name: "all_migrations", source: q.Class("Migration") })
