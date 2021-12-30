b8984ebd4aaa5ad59f59388d93fdb0ab11f04b55
shadid12 <shadidhaque2012@gmail.com>
2021-12-30 01:35:55 -0500

    refactor: 💡 remove unused componets react
    remove unused components

--- src/interactive-shell/messages/numbered-message.ts
+++
@@ -1,9 +0,0 @@
-// Copyright Fauna, Inc.
-// SPDX-License-Identifier: MIT-0
-
-export type MessageFun = (id?: number) => JSX.Element
-
-export interface NumberedMessage {
-  id: number
-  fun: MessageFun
-}
